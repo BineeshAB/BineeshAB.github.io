25d79ed454d0f10ddf55443f680cbc76194816ad
Bineesh Alakkal <bineeshalakkal22@gmail.com>
2021-10-21 10:28:45 +0530

    Updated Full Website Desgin

--- js/slider.js
+++ js/slider.js
@@ -1,64 +1,25 @@
-let five = window.matchMedia("(max-width: 540px)");
+let slider = document.getElementById('slider');
+let sliderBg = document.getElementById('sliderBg');
+let phoneView = window.matchMedia("(max-width: 540px)");
+let tabletView = window.matchMedia("(max-width: 768px)");
 
 function openNav(){
-	
-	document.getElementById("bars").style.display = "none";
-	document.getElementById("close").style.display = "block";
-	document.getElementById("slideIcon").style.display = "none";
-	document.getElementById("slider").style.width = "260px";
-
-	if(five.matches){
-		document.getElementById("home").style.marginLeft = "0px";
-		document.getElementById("about").style.marginLeft = "0px";
-		document.getElementById("profile").style.marginLeft = "0px";
-		document.getElementById("education").style.marginLeft = "0px";
-		document.getElementById("experience").style.marginLeft = "0px";
-		document.getElementById("project").style.marginLeft = "0px";
-		document.getElementById("footer").style.marginLeft = "0px";
-		document.getElementById("gallery").style.marginLeft = "0px";
+	sliderBg.style.width = "100vw";
+	if(phoneView.matches){
+		slider.style.width = "80vw";
+		slider.style.padding = "5px 10px";
+	}
+	else if(tabletView.matches){
+		slider.style.width = "40vw";
+		slider.style.padding = "5px 20px";
 	}
 	else{
-		document.getElementById("home").style.marginLeft = "260px";
-		document.getElementById("about").style.marginLeft = "260px";
-		document.getElementById("profile").style.marginLeft = "260px";
-		document.getElementById("education").style.marginLeft = "260px";
-		document.getElementById("experience").style.marginLeft = "260px";
-		document.getElementById("project").style.marginLeft = "260px";
-		document.getElementById("footer").style.marginLeft = "260px";
-		document.getElementById("gallery").style.marginLeft = "260px";
+		slider.style.width = "20vw";
+		slider.style.padding = "5px 20px";
 	}
-
-	
 }
 function closeNav(){
-
-	document.getElementById("bars").style.display = "block";
-	document.getElementById("close").style.display = "none";
-	document.getElementById("slider").style.width = "0px";
-	
-	if(five.matches){
-		document.getElementById("slideIcon").style.display = "none";
-
-		document.getElementById("home").style.marginLeft = "0px";
-		document.getElementById("about").style.marginLeft = "0px";
-		document.getElementById("profile").style.marginLeft = "0px";
-		document.getElementById("education").style.marginLeft = "0px";
-		document.getElementById("experience").style.marginLeft = "0px";
-		document.getElementById("project").style.marginLeft = "0px";
-		document.getElementById("footer").style.marginLeft = "0px";
-		document.getElementById("gallery").style.marginLeft = "0px";
-	}
-	else{
-		document.getElementById("slideIcon").style.display = "block";
-
-		document.getElementById("home").style.marginLeft = "85px";
-		document.getElementById("about").style.marginLeft = "85px";
-		document.getElementById("profile").style.marginLeft = "85px";
-		document.getElementById("education").style.marginLeft = "85px";
-		document.getElementById("experience").style.marginLeft = "85px";
-		document.getElementById("project").style.marginLeft = "85px";
-		document.getElementById("footer").style.marginLeft = "85px";
-		document.getElementById("gallery").style.marginLeft = "85px";
-	}
-
+	sliderBg.style.width = "0vw";
+	slider.style.width = "0px";
+	slider.style.padding = "0px";
 }
