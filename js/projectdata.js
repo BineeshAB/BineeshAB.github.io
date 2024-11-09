038b932c88bfca230b8c29dd7f18fccf912e4d0e
Bineesh Alakkal <bineeshalakkal22@gmail.com>
2024-11-09 20:08:46 +0530

    Changed the Design

---
+++ js/projectdata.js
@@ -0,0 +1,47 @@
+const myProjectData = [
+    {
+        id: 1,
+        name: "House Rental Website (FindAcres)",
+        description:
+            "A platform for users to find rental houses and apartments. It allows landlords to list their properties, and users can search based on different criteria.",
+        technologies: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"],
+        link: "http://findacres.epizy.com/",
+        imageSrc: "./project_images/findacers.png",
+    },
+    {
+        id: 2,
+        name: "Job Search Website (JobSearch)",
+        description:
+            "A job portal where users can search for job opportunities, filter by various job criteria, and apply for positions. It supports both job seekers and employers.",
+        technologies: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"],
+        link: "http://jobsearch.great-site.net/",
+        imageSrc: "./project_images/jobsearch.png",
+    },
+    {
+        id: 3,
+        name: "Car Racing Game (JSTrafficGame)",
+        description:
+            "A browser-based car racing game where players control a car and try to avoid traffic on the road while trying to reach the destination.",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        link: "https://bineeshab.github.io/SimpleJSTrafficGame/",
+        imageSrc: "./project_images/JSTrafficGame.png",
+    },
+    {
+        id: 4,
+        name: "Typing Speed Test (TypingTest)",
+        description:
+            "A simple typing speed test application that calculates the users typing speed based on the number of words typed within a time limit.",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        link: "https://bineeshab.github.io/TypingSpeedTest/",
+        imageSrc: "./project_images/Typing Test Image.png",
+    },
+    {
+        id: 5,
+        name: "Tic Tac Toe Game",
+        description:
+            "A simple two-player Tic Tac Toe game, where users can play the game against each other on the same device.",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        link: "https://bineeshab.github.io/tic-tac-toe/",
+        imageSrc: "./project_images/TicTacToe Image.png",
+    },
+];
---
+++ js/topthreeprojectlist.js
@@ -0,0 +1,72 @@
+let projectData = document.getElementById("project_list_three");
+
+for (let i = 0; i < 3; i++) {
+    let projectLink = myProjectData[i]["link"];
+    let projectImage = myProjectData[i]["imageSrc"];
+    let projectName = myProjectData[i]["name"];
+    let projectDescription = myProjectData[i]["description"];
+
+    myProjectList(
+        i,
+        projectLink,
+        projectImage,
+        projectName,
+        projectDescription
+    );
+}
+function myProjectList(
+    i,
+    projectLink,
+    projectImage,
+    projectName,
+    projectDescription
+) {
+    //A tag for Project Card
+    let projectCardDiv = document.createElement("a");
+    projectCardDiv.setAttribute("href", projectLink);
+    projectCardDiv.setAttribute("class", "project_card");
+    projectCardDiv.setAttribute("target", "_blank");
+    projectData.appendChild(projectCardDiv); // append to a tag (class = project_card)
+
+    //Div for Project Image
+    let projectImageDiv = document.createElement("div");
+    projectImageDiv.setAttribute("class", "project_image");
+    projectCardDiv.appendChild(projectImageDiv);
+
+    //Project Image
+    let projectImageTag = document.createElement("img");
+    projectImageTag.setAttribute("src", projectImage);
+    projectImageTag.setAttribute("alt", "");
+    projectImageDiv.appendChild(projectImageTag); // append to a div tag (class = project_image)
+
+    //Used Language
+    let projectLanguageUsedDiv = document.createElement("div");
+    projectLanguageUsedDiv.setAttribute("class", "used_language");
+    projectCardDiv.appendChild(projectLanguageUsedDiv); // append to a tag (class = project_card)
+
+    //For Loop for to print Used Programming Languages
+    for (let j = 0; j < myProjectData[i]["technologies"].length; j++) {
+        let languageUsed = myProjectData[i]["technologies"][j];
+        let languageID = languageUsed.toLowerCase() + "_color";
+        programmingLanguages(languageUsed, languageID);
+    }
+    // Function to create Div Elements for Programming Languages
+    function programmingLanguages(languageUsed, languageID) {
+        let languageNames = document.createElement("span");
+        languageNames.setAttribute("id", languageID);
+        languageNames.innerHTML = languageUsed;
+        projectLanguageUsedDiv.appendChild(languageNames); // append to a div tag (class = used_language)
+    }
+
+    //Project Name
+    let projectNameTag = document.createElement("div");
+    projectNameTag.setAttribute("class", "project_name");
+    projectNameTag.innerHTML = projectName;
+    projectCardDiv.appendChild(projectNameTag); // append to a tag (class = project_card)
+
+    //Project Description
+    let projectDescriptionTag = document.createElement("div");
+    projectDescriptionTag.setAttribute("class", "project_description");
+    projectDescriptionTag.innerHTML = projectDescription;
+    projectCardDiv.appendChild(projectDescriptionTag); // append to a tag (class = project_card)
+}
